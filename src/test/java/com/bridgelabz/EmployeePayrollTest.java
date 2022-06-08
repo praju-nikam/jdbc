9ca48fa40c1409642f753c096fe35c246a96cbc6
praju-nikam <sawanprajakta84@gmail.com>
2022-06-08 14:42:07 +0530

    Added Select Range By Date

--- src/test/java/com/bridgelabz/EmployeePayrollTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollTest.java
@@ -1,2 +1,21 @@
-package com.bridgelabz;public class EmployeePayrollTest {
+package com.bridgelabz;
+
+import com.bridgelabz.employeepayroll.EmployeePayrollService;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class EmployeePayrollTest
+{
+    EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+
+    @Test
+    public void givenUpdatingMayuriBasicPay_whenUpdate_ShouldReturnUpdatedPay() {
+        double BasicPay = 35000;
+        String Name = "Mayuri";
+        double salaryUpdated = employeePayrollService.updateBasicPay(Name, BasicPay);
+        Assert.assertEquals(BasicPay, salaryUpdated,35000);
+    }
+
+
 }
